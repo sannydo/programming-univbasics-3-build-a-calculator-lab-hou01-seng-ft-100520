8f30d95c4d566a53d6f0a8b503dc0bcb50b11b26
Sanny Do <sanny.do94@gmail.com>
2020-08-16 20:52:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-  addition = num2 + num1
+  addition == num1 + num2
 end
 
 def subtraction(num1, num2)
-  subtraction = num2 - num1
+  subtraction == num2 - num1
 end
 
 def division(num1, num2)
-  division = num2 / num1
+  division == num1 / num2
 end
 
 def multiplication(num1, num2)
-  multiplication = num1 * num2
+  multiplication == num1 * num2
 end
 
 def modulo(num1, num2)
-  modulo = num2 % num1
+  modulo == num1 % num2
 end
 
 def square_root(num)
-  square_root = num sqrt 
+  square_root == num sqrt 
 end

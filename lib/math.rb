5f1f4f3e8700dc5d1ff86df285148f8d79f1fa8d
Sanny Do <sanny.do94@gmail.com>
2020-08-16 20:51:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,13 @@
 def addition(num1, num2)
   addition = num2 + num1
-  puts addition
 end
 
 def subtraction(num1, num2)
   subtraction = num2 - num1
-  puts subtraction
 end
 
 def division(num1, num2)
   division = num2 / num1
-  puts division
 end
 
 def multiplication(num1, num2)

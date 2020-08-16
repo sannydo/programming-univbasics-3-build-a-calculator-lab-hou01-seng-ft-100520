982f08df727bd05289301c203e70faf8857feef0
Sanny Do <sanny.do94@gmail.com>
2020-08-16 20:48:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,24 @@
-def addition(num1, num2)
+def addition(num1 = 5, num2 = 4)
   addition = num2 + num1
   puts addition
 end
 
-def subtraction(num1, num2)
+def subtraction(num1 = 10, num2 = 5)
   subtraction = num2 - num1
   puts subtraction
 end
 
-def division(num1, num2)
+def division(num1 = 50, num2 = 2)
   division = num2 / num1
   puts division
 end
 
-def multiplication(num1, num2)
+def multiplication(num1 = 4, num2 = 30)
   multiplication = num1 * num2
   puts multiplication
 end
 
-def modulo(num1, num2)
+def modulo(num1 = 34, num2 = 5)
   modulo = num2 % num1
   puts modulo
 end
